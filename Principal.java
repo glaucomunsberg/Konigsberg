5209eda48dfd1e04136519447b45bb444449f930
Glauco Roberto Munsberg <glaucomunsberg@gmail.com>
2012-07-03 19:53:36 -0300

    Versão_0.2

--- Principal.java
+++ Principal.java
@@ -38,7 +38,7 @@ public class Principal{
      * 
      * @return void
      */
-    private void setValorMatriz(int linha, int coluna, int valor){
+    public void setValorMatriz(int linha, int coluna, int valor){
         this.matriz[linha][coluna] = valor;
     }
     
@@ -61,7 +61,7 @@ public class Principal{
      * 
      * @return void
      */
-    private void setNomeVertice(int vertice, String nome){
+    public void setNomeVertice(int vertice, String nome){
         verticeNome[vertice] = nome;
     }
     
@@ -76,20 +76,103 @@ public class Principal{
     }
     
     /**
+     * Método que retorna um array com os vertices vizinhos do
+     *  vertice passado
+     * @param int vertice
+     * 
+     * @return int[]vizinho
+     */
+    private int[] getVizinhosDaMatriz(int vertice){
+        int[] vizinhos = new int[1];
+        return vizinhos;
+    }
+    
+    /**
+     * Retorna a ordem topologica da matriz em questão
+     * 
+     * @return int[] ordemTopologica 
+     */
+    private int[] getOrdemTopologica(){
+        int[] ordem = new int[1];
+        return ordem;
+    }
+    
+    /**
      * Respeitando o JSON esse método retorna a string associada ao vértice de número ID
-     * se este existir, ou indica a ocorrência de falha
+     *  se este existir, ou indica a ocorrência de falha
      * @param int vertice
      * 
      * @return String verticeFormatoJSON
      */
-    private String getJSONid(int vertice){
+    public String getJSONid(int vertice){
         
-        if( vertice < this.tamanhoMaximo )
-        {
+        if( vertice < this.tamanhoMaximo ){
             return String.format("{\"vertice\":{\"ID\":%d, \"dado\":\"%s\", \"resposta\":\"sucesso\"}}", vertice+1,this.verticeNome[vertice] );
-        }else
-        {
+        }else{
             return String.format("{\"vertice\":{\"ID\":%d, \"dado\":\"\", \"resposta\":\"falha\"}}", vertice);
         }
+    }
+    
+    /**
+     * Método que remove da matriz o vertice, respeitando o modelo JSON
+     * @param int vertice
+     * @return String verticeRemovidoFormatoJSON
+     */
+    public String deleteJSONid(int vertice){
+        
+        if( vertice < this.tamanhoMaximo ){
+            return String.format("{\"delete\":{\"ID\":%d,\"resposta\":\"sucesso\"}}", vertice+1);
+        }else{
+            return String.format("{\"delete\":{\"ID\":%d,\"resposta\":\"falha\"}}", vertice+1);
+        }
+    }
+    
+    /**
+     * Método que retorna os vizinhos de vertice no formato JSON
+     * @param int vertice
+     * 
+     * @return String vizinhosFormatoJSON
+     */
+    public String vizinhoJSONid(int vertice){
+        
+        if( vertice < this.tamanhoMaximo){
+            String retorno = String.format("{\"vizinhos\":{\"ID\":%d, \"resposta\":\"sucesso\", \"vizinhos\":[", vertice+1);
+            int[] vizinho = this.getVizinhosDaMatriz(vertice);
+            for(int a=0; a < vizinho.length;a++){
+                retorno+=String.format("%d", vizinho[a]);
+                if( a+1 < vizinho.length ){
+                    retorno+=String.format(",", vizinho[a]);
+                }
+            }
+            retorno += "]}}";
+            return retorno;
+        }else{
+            return String.format("{\"vizinhos\":{\"ID\":%d, \"resposta\":\"falha\", \"vizinhos\":[]}}", vertice+1);
+        }
+    }
+    
+    public String conexaoJSONconexaoid(int id1, int id2){
+        if( id1 < this.tamanhoMaximo && id2 < this.tamanhoMaximo){
+            if( this.getValorMatriz(id1, id2) >= 0)
+            {
+                return String.format("{\"conexao\":{\"ID1\":%d, \"ID2\":%d, \"resposta\":\"sucesso\", \"conexao\":\"sim\"}}", id1+1, id2+1);
+            }else{
+                return String.format("{\"conexao\":{\"ID1\":%d, \"ID2\":%d, \"resposta\":\"sucesso\", \"conexao\":\"não\"}}", id1+1, id2+1);
+            }
+        }else{
+            return String.format("{\"conexao\":{\"ID1\":%d, \"ID2\":%d, \"resposta\":\"falha\", \"conexao\":\"\"}}", id1+1, id2+1);
+        }
+    }
+    
+    public String ordemTopologicaJSON(){
+        String retorno = "{\"ordemtop\":[";
+        int[] ordem = this.getOrdemTopologica();
+            for(int a=0; a < ordem.length;a++){
+                retorno+=String.format("%d", ordem[a]);
+                if( a+1 < ordem.length ){
+                    retorno+=String.format(",", ordem[a]);
+                }
+            }
+        return retorno+="]}"; 
     }
 }
